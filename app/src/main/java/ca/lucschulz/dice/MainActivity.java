fd8c7210f1274b3cb7771304c4d494a469bd1ad1
Luc Schulz <luc@lucschulz.com>
2018-12-29 14:54:53 -0500

    Added solid white dice to resources.

--- app/src/main/java/ca/lucschulz/dice/MainActivity.java
+++ app/src/main/java/ca/lucschulz/dice/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.action_about:
+            case R.id.menu_about:
                 showAboutInfo();
                 break;
             default:
