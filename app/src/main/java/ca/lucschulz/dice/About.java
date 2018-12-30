75ff190468d4f545c326dc39a148504cb1862a74
Luc Schulz <luc@lucschulz.com>
2018-12-29 23:02:26 -0500

    Release 1.3 update
    Made small changes to the about page. Includes link to GitHub repository.

--- app/src/main/java/ca/lucschulz/dice/About.java
+++ app/src/main/java/ca/lucschulz/dice/About.java
@@ -2,6 +2,11 @@ package ca.lucschulz.dice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 public class About extends AppCompatActivity {
 
@@ -9,5 +14,8 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        TextView tv = findViewById(R.id.text_Copyright);
+        tv.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
